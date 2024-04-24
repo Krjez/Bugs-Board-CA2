b1597fd744eb6b91c4bbea8b49d2cfb5c2cd6682
Jakub Polacek <Kuba.krtek@email.cz>
2024-04-24 16:08:51 +0200

    File reading start

--- main.cpp
+++ main.cpp
@@ -18,10 +18,45 @@ int main()
 
 }
 
+/**
+ * Board creation and populating with bugs
+ */
 void createBoard()
 {
-    Board board;
+    Board board = Board();
 
-    //TODO file reading
+    ifstream file("../bugs.txt");
+
+    if(file)
+    {
+        const char delimiter = ';';
+        while(!file.eof())
+        {
+            string type;
+            string value;
+            getline(file,type,delimiter);
+            if(type == "C")
+            {
+                int id; int x; int y; int direction; int size;
+                getline(file,value,delimiter);
+                id = stoi(value);
+                getline(file,value,delimiter);
+                x = stoi(value);
+                getline(file,value,delimiter);
+                y = stoi(value);
+                getline(file,value,delimiter);
+                direction = stoi(value);
+                getline(file,value,delimiter);
+                size = stoi(value);
+                Crawler c = Crawler(id, x, y, direction, size);
+
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        cout << "Error when loading in the file." << endl;
+    }
 
 }
